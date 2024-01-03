6701a6fd9ba857105eb9e55bd00132ca0f02b067
paoloumali <2522851+paoloumali@users.noreply.github.com>
2024-01-03 13:21:48 +0800

    replacements

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import tabula
 #dfs2 = tabula.read_pdf("https://github.com/tabulapdf/tabula-java/raw/master/src/test/resources/technology/tabula/arabic.pdf")
 
 # convert PDF into CSV file
-tabula.convert_into("pdfs/NCR_T-309-ND_122923.pdf", "output.csv", output_format="csv", pages='4')
+tabula.convert_into("pdfs/NCR_T-309-ND_122923.pdf", "output.csv", output_format="csv", pages='4-14')
 
 # convert all PDFs in a directory
 #tabula.convert_into_by_batch("input_directory", output_format='csv', pages='all')
